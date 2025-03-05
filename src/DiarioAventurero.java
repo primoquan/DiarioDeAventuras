655a4a8511f95e459d2adf93b6eaca863bffef45
Luis Quan <primoquan@gmail.com>
2025-03-04 19:08:51 -0600

    Metodos de acciones de menu

--- src/DiarioAventurero.java
+++ src/DiarioAventurero.java
@@ -1,4 +1,56 @@
+//Universidad Da Vinci de Guatemala
+//Estructura de Datos
+//Ingeniero Brandon Chitay
+//Proyecto Diario de aventuras
+//Luis Quan
+//201927151
+
 package src;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class DiarioAventurero {
+    //Creamos las listas que vamos a usar para nuestro diario
+    private LinkedList<String> MisionesPendientes;
+    private LinkedList<String> MisionesCompletadas;
+    private Scanner scanner;
+
+    public DiarioAventurero() {
+        MisionesPendientes = new LinkedList<>();
+        MisionesCompletadas = new LinkedList<>();
+        scanner = new Scanner(System.in);
+    }
+
+    //Creamos nuestros metodos de las acciones del menu
+
+    //Creamos nuestro metodo Registrar Mision
+    public void RegistrarMision(){
+
+    }
+
+    //Creamos nuestro metodo Mostrar misiones pendientes
+    public void MostarMisionesPendientes(){
+
+    }
+
+    //Creamos nuestro metodo Completar misiones
+    public void CompletarMision(){
+
+    }
+
+    //Creamos nuestro metodo Mostrar misiones Completadas
+    public void MostrarMisionesCompletadas(){
+
+    }
+
+    //Creamos nuestro metodo Eliminar misiones
+    public void EliminarMision(){
+
+    }
+
+    //Creamos nuestro metodo Mostrar misiones pendientes
+    public void BuscarMision(){
+
+    }
 }
